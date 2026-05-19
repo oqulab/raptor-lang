63f7cdf078ace37ac49160bbb51615d6b6aa0f79
RAONE <rauan.satanbek@gmail.com>
2026-05-19 09:58:59 +0500

    mavan settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,21 +3,52 @@ plugins {
     kotlin("jvm") version "2.1.0"
     kotlin("plugin.serialization") version "2.1.0"
     `maven-publish`
+    signing
 }
 
 group = "kz.oqulab"
 version = "1.0.0"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+    withSourcesJar()
+    withJavadocJar()
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+    }
+    jvmToolchain(11)
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+}
+
 publishing {
     publications {
         create<MavenPublication>("release") {
             from(components["java"])
+
             pom {
                 name.set("Raptor Language")
                 description.set("Lightweight embeddable programming language interpreter written in Kotlin")
                 url.set("https://github.com/oqulab/raptor-lang")
-                licenses { license { name.set("MIT"); url.set("https://opensource.org/licenses/MIT") } }
-                developers { developer { id.set("oqulab"); name.set("Rauan Satanbek"); email.set("hello@oqulab.kz") } }
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("oqulab")
+                        name.set("Rauan Satanbek")
+                        email.set("hello@oqulab.kz")
+                    }
+                }
                 scm {
                     connection.set("scm:git:git://github.com/oqulab/raptor-lang.git")
                     developerConnection.set("scm:git:ssh://github.com/oqulab/raptor-lang.git")
@@ -26,29 +57,27 @@ publishing {
             }
         }
     }
+
     repositories {
         maven {
-            name = "sonatype"
-            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            name = "sonatypeCentral"
+            url = uri("https://central.sonatype.com/api/v1/publisher/deployments")
             credentials {
-                username = project.findProperty("SONATYPE_USERNAME") as String? ?: ""
-                password = project.findProperty("SONATYPE_PASSWORD") as String? ?: ""
+                username = project.findProperty("sonarCentralUsername") as String? ?: ""
+                password = project.findProperty("sonarCentralPassword") as String? ?: ""
             }
         }
     }
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
+signing {
+    val signingKey = project.findProperty("signing.key") as String?
+    val signingPassphrase = project.findProperty("signing.passphrase") as String?
 
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+    // Если свойства заполнены, настраиваем ин-мемори подпись
+    if (!signingKey.isNullOrEmpty() && !signingPassphrase.isNullOrEmpty()) {
+        useInMemoryPgpKeys(signingKey, signingPassphrase)
     }
-}
 
-dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    sign(publishing.publications["release"])
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,9 @@
 rootProject.name = "raptor-lang"
+dependencyResolutionManagement {
+    // Меняем FAIL_ON_PROJECT_REPOS на PREFER_PROJECT
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
