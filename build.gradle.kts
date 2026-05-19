6f8548d5db271e6c70d3a91383d959cab1f9d622
RAONE <rauan.satanbek@gmail.com>
2026-05-19 07:14:00 +0500

    Standalone build: Gradle wrapper, direct plugin versions, .gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
-    alias(libs.plugins.kotlin.serialization)
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.serialization") version "2.1.0"
     `maven-publish`
 }
 
@@ -12,27 +12,12 @@ publishing {
     publications {
         create<MavenPublication>("release") {
             from(components["java"])
-            
             pom {
                 name.set("Raptor Language")
                 description.set("Lightweight embeddable programming language interpreter written in Kotlin")
                 url.set("https://github.com/oqulab/raptor-lang")
-                
-                licenses {
-                    license {
-                        name.set("MIT")
-                        url.set("https://opensource.org/licenses/MIT")
-                    }
-                }
-                
-                developers {
-                    developer {
-                        id.set("oqulab")
-                        name.set("Rauan Satanbek")
-                        email.set("hello@oqulab.kz")
-                    }
-                }
-                
+                licenses { license { name.set("MIT"); url.set("https://opensource.org/licenses/MIT") } }
+                developers { developer { id.set("oqulab"); name.set("Rauan Satanbek"); email.set("hello@oqulab.kz") } }
                 scm {
                     connection.set("scm:git:git://github.com/oqulab/raptor-lang.git")
                     developerConnection.set("scm:git:ssh://github.com/oqulab/raptor-lang.git")
@@ -41,25 +26,23 @@ publishing {
             }
         }
     }
-    
     repositories {
         maven {
             name = "sonatype"
             url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
             credentials {
-                // Add in ~/.gradle/gradle.properties:
-                // SONATYPE_USERNAME=your_username
-                // SONATYPE_PASSWORD=your_password
                 username = project.findProperty("SONATYPE_USERNAME") as String? ?: ""
                 password = project.findProperty("SONATYPE_PASSWORD") as String? ?: ""
             }
         }
     }
 }
+
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
 }
+
 kotlin {
     compilerOptions {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
@@ -67,5 +50,5 @@ kotlin {
 }
 
 dependencies {
-    implementation(libs.kotlinx.serialization.json)
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 }
